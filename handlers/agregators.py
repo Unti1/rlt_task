bf5df6518cb4f1e4e09f5ea31ee3f8e7a4fa0a65
Unti1 <kostyuchenko.work@gmail.com>
2024-05-17 15:40:53 +0300

    Changes to be committed: 	new file:   Dockerfile -- for docker 	new file:   agregator_test.py -- adding pytest from word file 	new file:   docker-compose.yml -- for docker 	modified:   handlers/agregators.py -- main handler of agregating 	modified:   pyproject.toml 	modified:   tools/db_agregator.py -- main func of agregating

--- handlers/agregators.py
+++ handlers/agregators.py
@@ -4,5 +4,21 @@ router = Router()
 
 @router.message(F.text)
 async def process_json(message: types.Message):
-    pass
+    from tools.db_agregator import aggregate_salaries
+    try:
+        # JSON parse
+        data = json.loads(message.text)
+        # agregating
+        result = await aggregate_salaries(**data)
+        # send result
+        await message.answer(json.dumps(result, ensure_ascii=False))
     
+    except json.JSONDecodeError:
+        await message.reply("Пожалуйста, отправьте данные в правильном формате JSON.")
+    
+    except KeyError as e:
+        await message.reply(f"Отсутствует необходимый ключ в данных: {e}")
+    
+    except Exception as e:
+        await message.reply(f"Произошла ошибка: {e}")
+        logging.error(traceback.format_exc())
--- tools/db_agregator.py
+++ tools/db_agregator.py
@@ -14,5 +14,39 @@ async def aggregate_salaries(dt_from, dt_upto, group_type):
         'month': '%Y-%m-01T00:00:00'
     }[group_type]
 
+    pipeline = [
+        {"$match": {"dt": {"$gte": datetime.fromisoformat(dt_from), "$lte": datetime.fromisoformat(dt_upto)}}},
+        {"$group": {"_id": {"$dateToString": {"format": group_format, "date": "$dt"}}, "totalAmount": {"$sum": "$value"}}},
+        {"$sort": {"_id": 1}},
+        {"$project": {"_id": 0, "label": "$_id", "totalAmount": 1}}
+    ]
+
+    cursor = collection.aggregate(pipeline)
+    dataset, labels = [], []
+    async for doc in cursor:
+        labels.append(doc['label'])
+        dataset.append(doc['totalAmount'])
+
+    # Дополнение недостающих дат с нулевыми значениями
+    current_date = datetime.fromisoformat(dt_from)
+    end_date = datetime.fromisoformat(dt_upto)
+    filled_dataset, filled_labels = [], []
+    
+    while current_date <= end_date:
+        str_date = current_date.strftime(group_format)
+        if str_date in labels:
+            index = labels.index(str_date)
+            filled_dataset.append(dataset[index])
+        else:
+            filled_dataset.append(0)
+        filled_labels.append(str_date)
+        
+        if group_type == 'hour':
+            current_date += timedelta(hours=1)
+        elif group_type == 'day':
+            current_date += timedelta(days=1)
+        elif group_type == 'month':
+            current_date += relativedelta(months=+1)
+    
     return {'dataset': filled_dataset, 'labels': filled_labels}
 
